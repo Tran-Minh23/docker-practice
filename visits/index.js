d6c33419b7092c8dd844eb46c35fd881259e74e0
DESKTOP-0FQEQD0\batha <bathackcf@gmail.com>
2023-11-26 22:56:34 +0700

    fix docker issue

--- visits/index.js
+++ visits/index.js
@@ -2,17 +2,32 @@ const express = require('express');
 const redis = require('redis');
 
 const app = express();
-const client = redis.createClient({
-  host: 'redis-server',
-  port: 6379
-});
-client.set('visits', 0);
 
-app.get('/', (req, res) => {
-  client.get('visits', (err, visits) => {
-    res.send('Number of visits is ' + visits);
-    client.set('visits', parseInt(visits) + 1);
-  });
+let client;
+(async () => {
+  client = redis.createClient({
+    socket: {
+       port: 6379,
+       host: 'redis-server'
+    }
+});
+
+  client.on("error", (error) => console.error(`Error : ${error}`));
+
+  await client.connect();
+  await client.set('visits', 0);
+})();
+
+app.get('/', async (req, res) => {
+  // client.get('visits', (err, visits) => {
+  //   res.send('Number of visits is ' + visits);
+  //   client.set('visits', parseInt(visits) + 1);
+  // });
+  const val = await client.get("visits")
+  const valR = val;
+
+  await client.set('visits', parseInt(val) + 1);
+  res.send('Number of visits is ' + valR);
 });
 
 app.listen(8081, () => {
